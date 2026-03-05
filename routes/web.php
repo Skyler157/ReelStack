6a6a48be8f87b74cb44dfb12bf4ec8a9db78f1af
Leah Gitau <Leah.Gitau@CRAFTSILICON.LOCAL>
2026-03-05 10:00:28 +0300

    healthcheck

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('reelstack');
 })->name('home');
 
+Route::get('/healthz', function () {
+    return response('ok', 200)->header('Content-Type', 'text/plain');
+});
+
 Route::view('/about-us', 'about')->name('about');
 Route::view('/privacy-policy', 'privacy')->name('privacy');
 Route::view('/terms-of-use', 'terms')->name('terms');
